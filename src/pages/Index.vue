3d447dc05026d5b6b7a124ca6f710d069418f85d
sapphi-red <green@sapphi.red>
2021-03-04 22:34:41 +0900

    Fix vue logo

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,21 @@
 <template>
   <routes />
   <div>Index Page</div>
-  <img alt="Vue logo" src="/@/assets/logo.png" />
+  <img alt="Vue logo" :src="logoSrc" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Routes from '/@/components/Routes.vue'
+import logoSrc from '/@/assets/logo.png'
 
 export default defineComponent({
   name: 'Index',
   components: {
     Routes
+  },
+  setup() {
+    return { logoSrc }
   }
 })
 </script>
